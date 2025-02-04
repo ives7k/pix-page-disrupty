39e0c14b5335b4a0ceed322c7a433255c7ba252b
ives7k <165990522+ives7k@users.noreply.github.com>
2025-02-03 22:45:04 -0300

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-function getUTMParams(): { [key: string]: string } {
-  const params: { [key: string]: string } = {};
-  const queryString = window.location.search.substring(1);
-  const urlParams = new URLSearchParams(queryString);
-
-  for (const [key, value] of urlParams.entries()) {
-    params[key] = value;
-  }
-  return params;
-}
-
-const utmParams = getUTMParams();
-
-if (utmParams) {
-  localStorage.setItem('utmParams', JSON.stringify(utmParams));
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
